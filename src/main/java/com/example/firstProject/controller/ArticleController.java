d91620ccb7b508af98c574afae8ab599be819080
zzoingsu <nininick@naver.com>
2026-07-13 21:01:34 +0900

    CRUD day2

--- src/main/java/com/example/firstProject/controller/ArticleController.java
+++ src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -1,12 +1,17 @@
 package com.example.firstProject.controller;
 
 import com.example.firstProject.dto.ArticleForm;
+import com.example.firstProject.entity.Article;
+import com.example.firstProject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepossitory;
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -15,8 +20,12 @@ public class ArticleController {
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
         // 1. DTO를 엔티티로 전환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
         // 2. repository로 엔티티를 DB에 저장
-        return "redirect:/articles/new";
+        Article saved = articleRepossitory.save(article);
+        System.out.println(saved.toString());
+        return "";
     }
 
 }
--- src/main/java/com/example/firstProject/dto/ArticleForm.java
+++ src/main/java/com/example/firstProject/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.firstProject.dto;
 
+import com.example.firstProject.entity.Article;
+
 public class ArticleForm {
     private String title;
     private String content;
@@ -15,6 +17,10 @@ public class ArticleForm {
                 ", content='" + content +'\'' +
                 "}";
     }
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+
 
 
 }
