fe808af561a95fd69c83ac261077fd68bd3967f7
zzoingsu <nininick@naver.com>
2026-07-16 16:22:17 +0900

    CRUD day3

--- src/main/java/com/example/firstProject/controller/ArticleController.java
+++ src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstProject.controller;
 import com.example.firstProject.dto.ArticleForm;
 import com.example.firstProject.entity.Article;
 import com.example.firstProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -19,14 +21,16 @@ public class ArticleController {
 
     @PostMapping ("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        // System.out.println(form.toString());
         // 1. DTO를 엔티티로 전환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        // System.out.println(article.toString());
         // 2. repository로 엔티티를 DB에 저장
         Article saved = articleRepossitory.save(article);
         System.out.println(saved.toString());
-        return "";
+        return "articles/new";
     }
 
 }
