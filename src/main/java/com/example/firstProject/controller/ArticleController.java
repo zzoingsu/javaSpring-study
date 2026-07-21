7a595120986f4c41baa6d520d67a27be21be5dd8
zzoingsu <nininick@naver.com>
2026-07-21 17:48:56 +0900

    CRUD-Reading

--- src/main/java/com/example/firstProject/controller/ArticleController.java
+++ src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.firstProject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -33,4 +38,24 @@ public class ArticleController {
         return "articles/new";
     }
 
+@GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id=" + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepossitory.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepossitory.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
+    }
 }
